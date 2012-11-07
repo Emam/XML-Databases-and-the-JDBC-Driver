90adc56bab07c85d96cc28898002774e271947b5
houssainy <mohamed.houssainy@hotmail.com>
2012-11-07 19:19:12 +0200

    Houssainy MAster

---
+++ src/HoussainyMaster.java
@@ -0,0 +1,9 @@
+
+public class HoussainyMaster {
+
+	public HoussainyMaster() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Iam the master :D");
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Houssainy h = new Houssainy();
+		HoussainyMaster ho = new HoussainyMaster();
 	}
 
 }
