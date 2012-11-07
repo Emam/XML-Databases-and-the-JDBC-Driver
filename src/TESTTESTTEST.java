d4557ba5f43bde60b3378b010056f56147108938
houssainy <mohamed.houssainy@hotmail.com>
2012-11-07 19:44:07 +0200

    Test 3'alasa

--- src/TESTTESTTEST.java
+++ src/TESTTESTTEST.java
@@ -2,5 +2,6 @@
 public class TESTTESTTEST {
 	public TESTTESTTEST(){
 		System.out.println("6 tmaren l estedart el mo2a5ra xD");
+		System.out.println("YA ragel :D");
 	}
 }
---
+++ src/ahoGalasaKeda.java
@@ -0,0 +1,9 @@
+
+public class ahoGalasaKeda {
+
+	public ahoGalasaKeda() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Aho 3'alasa Keda ^_^");
+	}
+
+}
