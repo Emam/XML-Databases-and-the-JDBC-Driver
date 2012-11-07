74713b8f2a10bf0954ec3bd0cadb84691799c37e
houssainy <mohamed.houssainy@hotmail.com>
2012-11-07 19:55:57 +0200

    Conflict Houssainy

--- src/beshrTest.java
+++ src/beshrTest.java
@@ -2,5 +2,8 @@
 public class beshrTest {
 	public static void test() {
 		System.out.println("eh ya regala?");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Beshr heheheheheh");
+		}
 	}
 }
